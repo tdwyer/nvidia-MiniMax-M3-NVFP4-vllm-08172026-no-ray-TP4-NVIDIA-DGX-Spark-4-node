68d4e65e9d68b14881175fb388f544d365cae59c
Thomas Dwyer <github@tomd.tel>
2026-08-23 19:41:18 -0500

    Add fastokens to the vllm_docker_build.sh

--- vllm_docker_build.sh
+++ vllm_docker_build.sh
@@ -2,6 +2,10 @@ git clone https://github.com/vllm-project/vllm.git
 
 cd vllm
 
+echo "RUN pip install --no-cache-dir fastokens" >> docker/Dockerfile
+echo "ENV VLLM_USE_FASTOKENS=1" >> docker/Dockerfile
+echo 'ENTRYPOINT ["sleep", "infinity"]' >> docker/Dockerfile
+
 docker build \
   -f docker/Dockerfile \
   --build-arg BUILD_OS=manylinux \
